60e505ed8b07f84dd94efb968147d750032aaa7e
Cassia Bernardo <cassiabernardo@Cassias-MacBook-Air.local>
2019-11-27 08:42:04 +0000

    addition of templates

---
+++ data.js
@@ -0,0 +1,43 @@
+module.exports = [{
+    id: "W6NZfCO5SIk",
+    title: "Learn JavaScript with Mosh",
+    duration: "57 min",
+    price: "free",
+    featured: true
+  },
+
+  {
+    id: "W6NZfCO5SIk",
+    title: "Learn JavaScript with Mosh",
+    duration: "57 min",
+    price: "free"
+  },
+
+  {
+    id: "W6NZfCO5SIk",
+    title: "Learn JavaScript with Mosh",
+    duration: "57 min",
+    price: "free"
+  },
+
+  {
+    id: "W6NZfCO5SIk",
+    title: "Learn JavaScript with Mosh",
+    duration: "57 min",
+    price: "free"
+  },
+
+  {
+    id: "W6NZfCO5SIk",
+    title: "Learn JavaScript with Mosh",
+    duration: "57 min",
+    price: "free"
+  },
+
+  {
+    id: "W6NZfCO5SIk",
+    title: "Learn JavaScript with Mosh",
+    duration: "57 min",
+    price: "free"
+  }
+]
--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+ 
 const server = express()
+const videos = require("./data")
 
+server.use(express.static('public'))
+
+// let's config nunjucks
+server.set('view engine', 'njk')
+nunjucks.configure('views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+})
+
+// let's config server
 server.get('/', function(req, res) {
-  return res.send("hello")
+  const about = {
+    avatar_url: "https://media.licdn.com/dms/image/C4D03AQFDa4Xk_VjWIw/profile-displayphoto-shrink_200_200/0?e=1579132800&v=beta&t=LNYsTS5pIJcmZ4dCDbz-tbtzcjYAEGcnv6mTfGSS-l8",
+    name: "Cassia Bernardo",
+    role: "Web Developer",
+    description: "Passionate about building beautiful websites",
+    links: [
+      { name: "Github", url: "https://github.com/cah90" },
+      { name: "Linkedin", url: "https://www.linkedin.com/in/cassia-bernardo/"}
+    ]
+
+  }
+  return res.render('about', { about: about })
+})
+
+server.get('/portfolio', function(req, res) {
+  return res.render('portfolio', { items: videos })
 })
 
 server.listen(5000, function(){
